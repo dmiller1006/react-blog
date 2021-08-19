a85b93dd3968667cd7b3cc42bebe2095a0600d6c
dmiller1006 <dan.miller1006@gmail.com>
2021-08-19 08:32:12 -0400

    lesson10

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,22 @@
 import { useState } from 'react'
 
 const Home = () => {
-  // let name = 'Max'
-  // array deconstructing to handle two values from useState useState => value, function to rerender
-  const [name,setName] = useState('max')
-  const [age, setAge] = useState(25)
-
-  const handleClick = () => {
-    setName('sergio')
-    setAge(34)
-  }
+  const [blogs, setBlogs] = useState(
+    [
+      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]
+  )
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>His name is { name } and he is { age } years old.</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by: {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
